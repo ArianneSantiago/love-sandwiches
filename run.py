eaff129b821a701e28c2ead0aa092d61fbc640d9
Arianne Santiago <ariannesantiago05@gmail.com>
2024-02-16 11:41:37 +0000

    Add users request loop to repeat for data until provided is valid

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ def get_sales_data():
     """
     Get Sales figures input from the user
     """
+while True:
 
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, seperated by commas.")
@@ -27,7 +28,12 @@ def get_sales_data():
     data_str = input("Enter your data here: ")
  
     sales_data = data_str.split(",")
-    validate_data(sales_data)
+
+    if validate_data(sales_data):
+        print("Data is valid!")
+        break
+
+return sales_data
 
 def validate_data(values):
     """
@@ -43,5 +49,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+data = get_sales_data()
